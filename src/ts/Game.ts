1db4245acfd9cdf171eb34eb22f170f2cb7e2c02
James Skemp <strivinglife@gmail.com>
2018-09-01 21:44:49 -0500

    Add disabled functionality to scale game to fill the entire browser

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -24,6 +24,28 @@ export default class Game extends Phaser.Game {
 	}
 };
 
+/**
+ * Workaround for inability to scale in Phaser 3.
+ * From http://www.emanueleferonato.com/2018/02/16/how-to-scale-your-html5-games-if-your-framework-does-not-feature-a-scale-manager-or-if-you-do-not-use-any-framework/
+ */
+function resize() {
+	let canvas = document.querySelector("canvas");
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	let wratio = width / height;
+	let ratio = Number(gameConfig.width) / Number(gameConfig.height);
+	if (wratio < ratio) {
+		canvas.style.width = width + "px";
+		canvas.style.height = (width / ratio) + "px";
+	} else {
+		canvas.style.width = (height * ratio) + "px";
+		canvas.style.height = height + "px";
+	}
+}
+
 window.onload = () => {
 	var game = new Game(gameConfig);
+	// Uncomment the following two lines if you want the game to scale to fill the entire page, but keep the game ratio.
+	//resize();
+	//window.addEventListener('resize', resize, true);
 };
