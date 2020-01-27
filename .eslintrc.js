08b0e74380393662b693012419fd09746a08ac7b
James Skemp <strivinglife@gmail.com>
2020-01-26 20:49:32 -0600

    Update eslint rules with old tslint variants
    Also fix a few eslint-reported issues.
    
    Update TypeScript to output ES2017 code, which seems well supported. (ES2019 looks to be supported too.)

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,15 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/eslint-recommended',
 		'plugin:@typescript-eslint/recommended',
-	]
+	],
+	rules: {
+		"indent": ["warn", "tab"],
+		"no-console": ["warn"],
+		"no-multiple-empty-lines": ["warn", { "max": 1 }]
+	},
+	env: {
+		"browser": true,
+		"commonjs": true,
+		"es2017": true
+	}
 };
